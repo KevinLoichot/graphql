50c4f94dafe12f75997a323ebee5e2e5a90d8146
kloichot <loichot.kevin@hotmail.com>
2026-03-18 11:34:39 +0100

    feat(graphql): add base graphqlQuery client with Bearer auth

--- js/queries.js
+++ js/queries.js
@@ -1,1 +1,17 @@
 const GRAPHQL_URL = 'https://zone01normandie.org/api/graphql-engine/v1/graphql';
+
+async function graphqlQuery(query, variables = {}) {
+  const token = localStorage.getItem('jwt');
+
+  const response = await fetch(GRAPHQL_URL, {
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer ${token}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ query, variables }),
+  });
+
+  const result = await response.json();
+  return result.data;
+}
